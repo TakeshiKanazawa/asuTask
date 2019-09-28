d615e66b6dfc61b484fa77271399d131c2e25a09
Takeshi Kanazawa <oseans@hotmail.co.jp>
2019-09-28 22:24:45 +0900

    second commit

---
+++ asuTask/NextViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NextViewController.swift
+//  asuTask
+//
+//  Created by 金澤武士 on 2019/09/28.
+//  Copyright © 2019 tk. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+          dismiss(animated: true, completion:nil)
+    }
+    
+
+}
