cee3097c3d760305fd9ac23ea7502948d580de5c
Takeshi Kanazawa <oseans@hotmail.co.jp>
2019-10-01 23:38:02 +0900

    8th commit

--- asuTask/ViewController.swift
+++ asuTask/ViewController.swift
@@ -8,91 +8,99 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
     
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var tableView: UITableView!
-    
+
     var indexNumber = Int()
     
+
     //リターンキーが押されたかどうかを判定する
     var textFieldTouchReturnKey = false
-    
-    
+
     var textArray = [String]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.delegate = self
         tableView.dataSource = self
         textField.delegate = self
-        
+
     }
 
     //セクションのセルの数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return textArray.count
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
-    
+
     }
     //セルの中身
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.textLabel?.text  = textArray[indexPath.row]
+
+        cell.textLabel?.text = textArray[indexPath.row]
 //        cell.imageView?.image = UIImage(named:)
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        //タップした時にその配列の番号の中身を取り出して値を渡す
-        let nextVC = storyboard?.instantiateViewController(withIdentifier: "next") as! NextViewController
-        
-            nextVC.taskNameString = textArray[indexPath.row]
-        
-        navigationController?.pushViewController(nextVC, animated: true)
+
+        textFieldTouchReturnKey = false
+        indexNumber = indexPath.row
+
+
     }
-    
+
     //セルの高さ
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-        return view.frame.size.height/6
-        
+
+        return view.frame.size.height / 6
+
     }
-    
-        //値を次の画面へ渡す処理
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    //値を次の画面へ渡す処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //セルがタップされた状態(タスク詳細画面の表示)
+
+        if (segue.identifier == "next") &&
+            textFieldTouchReturnKey == false {
+            //タップした時にその配列の番号の中身を取り出して値を渡す
+            let nextVC = storyboard?.instantiateViewController(withIdentifier: "next") as! NextViewController
+
+            //変数名.が持つ変数 =  渡したいものが入った変数
+            nextVC.taskNameString = textArray[indexNumber]
+
+        } else if (segue.identifier == "next") && textFieldTouchReturnKey == true {
+            //タップした時にその配列の番号の中身を取り出して値を渡す
+            let nextVC = storyboard?.instantiateViewController(withIdentifier: "next") as! NextViewController
+            nextVC.taskNameString = textField.text!
             
-            if (segue.identifier == "next") {
-                let subVC: NextViewController = segue.destination as! NextViewController
-                //変数名.が持つ変数 =  渡したいものが入った変数
-                subVC.taskNameString = textField.text!
-                
-                
-            }
         }
-    
+    }
+
     //returnキーが押された時に発動するメソッド
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
         
-        //押された
+        //リターンキーが押された
         textFieldTouchReturnKey = true
         textArray.append(textField.text!)
         textField.resignFirstResponder()
         textField.text = ""
-//        tableView.reloadData()
         //タスク作成画面へ遷移させる
         performSegue(withIdentifier: "next", sender: nil)
-        
+
         return true
-        
+
     }
 }
+
 
