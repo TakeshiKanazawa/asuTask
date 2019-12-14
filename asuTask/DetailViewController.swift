7349764ced4a181ad606062af6fd72e071506217
Takeshi Kanazawa <oseans@hotmail.co.jp>
2019-12-14 22:34:38 +0900

    25th commit #18のIssue作業

--- asuTask/DetailViewController.swift
+++ asuTask/DetailViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    //タスク名のインスタンス
     var taskNameString = String()
+    //タスク時刻のインスタンス
+    var taskTimeString = String()
+    //タスク優先度のインスタンス
+    var taskPriorityString = String()
+    
     @IBOutlet weak var taskNameButton: UIButton!
     
     @IBOutlet weak var taskPlannedTimeButton: UIButton!
@@ -19,12 +25,19 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-       //タスク名の表示
-        taskNameButton.setTitle(taskNameString, for:.normal)
-
+        //タスク名の表示
+         taskNameButton.setTitle(taskNameString, for:.normal)
+        //タスク時刻の表示
+        taskPlannedTimeButton.setTitle(taskTimeString, for: .normal)
+        //タスク優先度の表示
+        taskPriorityButton.setTitle(taskPriorityString, for: .normal)
     }
     
-
+    //戻るボタン
+    @IBAction func backButton(_ sender: Any) {
+              dismiss(animated: true, completion: nil)
+    }
+    
 
 
 }
